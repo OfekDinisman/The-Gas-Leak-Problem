04dc634dcf6e15435b6e350c899fec70fba666d0
michael-glu <glusteinm@gmail.com>
2021-08-03 21:32:08 +0300

    initial

---
+++ src/const.py
@@ -0,0 +1,8 @@
+SFS_URL = "https://wise-fox-7h5va4-dev-ed.lightning.force.com/"
+
+#REST API URLs
+SERVICE_APPOINTMENT = "services/data/v52.0/sobjects/ServiceAppointment/"
+SERVICE_RESOURCE = "services/data/v52.0/sobjects/ServiceResource/"
+SERVICE_TERRITORY = "services/data/v52.0/sobjects/ServiceTerritory/"
+SERVICE_TERRITORY_MEMBER = "services/data/v52.0/sobjects/ServiceTerritoryMember/"
+MAP_POLYGON = "services/data/v52.0/sobjects/FSL_Polygon_c"
